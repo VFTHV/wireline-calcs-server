57385c95e6763b1c75e9d5444931000bdcae15fd
Vadim Fattakhov <vadfthv@gmail.com>
2024-04-22 20:58:31 -0600

    submit problem controller

--- controllers/formController.js
+++ controllers/formController.js
@@ -7,6 +7,7 @@ const submitProblem = async (req, res) => {
   res.send({ msg: 'form submitted' });
   res.send({ msg: 'form submitted' });
   res.send({ msg: 'form submitted' });
+  res.send({ msg: 'form submitted' });
 };
 
 module.exports = { submitProblem };
