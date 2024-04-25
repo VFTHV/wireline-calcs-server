7779374778608ade487f37dc8ea8d953fd5a3a66
Vadim Fattakhov <vadfthv@gmail.com>
2024-04-24 19:05:21 -0600

    corrected error handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -7,8 +7,6 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || 'Something went wrong try again later',
   };
 
-  // }
-
   if (err.name === 'ValidationError') {
     customError.msg = Object.values(err.errors)
       .map((item) => item.message)
