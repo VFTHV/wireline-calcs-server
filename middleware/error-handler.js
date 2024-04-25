98f11d4cee4adad99a0fa52114f9fea68fcadc93
Vadim Fattakhov <vadfthv@gmail.com>
2024-04-24 19:04:46 -0600

    corrected error handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,7 +2,6 @@ const CustomAPIError = require('../errors/custom-api');
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  // if (err instanceof CustomAPIError) {
   //   return res.status(err.statusCode).json({ msg: err.message });
   // }
 
