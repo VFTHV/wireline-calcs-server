66b6354d67ae630bdcf85a3246cd5dd36e240902
Vadim Fattakhov <vadfthv@gmail.com>
2024-04-25 18:16:28 -0600

    refactor submitProblm controller

--- controllers/formController.js
+++ controllers/formController.js
@@ -1,5 +1,21 @@
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+
 const submitProblem = async (req, res) => {
-  res.send({ msg: 'form submitted' });
+  const { name, issue, description } = req.body;
+
+  if (!name || !issue || !description) {
+    throw new CustomError.BadRequestError('Please provide all information');
+  }
+
+  res
+    .status(StatusCodes.OK)
+    .json({
+      name,
+      issue,
+      description,
+      msg: 'Success. Thanks for your feedback!',
+    });
 };
 
 module.exports = { submitProblem };
