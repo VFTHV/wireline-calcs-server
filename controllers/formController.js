77828e156e9bcbe9148fbfe737941965abe0ce08
Vadim Fattakhov <vadfthv@gmail.com>
2024-04-25 18:25:12 -0600

    delay function for dev only

--- controllers/formController.js
+++ controllers/formController.js
@@ -4,18 +4,27 @@ const CustomError = require('../errors');
 const submitProblem = async (req, res) => {
   const { name, issue, description } = req.body;
 
+  // FOR DEV ONLY!!!
+  // const wait = (delay) => {
+  //   return new Promise((res) => {
+  //     setTimeout(() => {
+  //       res();
+  //     }, delay);
+  //   });
+  // };
+
+  // await wait(2000);
+
   if (!name || !issue || !description) {
     throw new CustomError.BadRequestError('Please provide all information');
   }
 
-  res
-    .status(StatusCodes.OK)
-    .json({
-      name,
-      issue,
-      description,
-      msg: 'Success. Thanks for your feedback!',
-    });
+  res.status(StatusCodes.OK).json({
+    name,
+    issue,
+    description,
+    msg: 'Success. Thanks for your feedback!',
+  });
 };
 
 module.exports = { submitProblem };
