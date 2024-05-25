22d0b968da72df89f411ee89263442bcdf818768
Vadim Fattakhov <vadfthv@gmail.com>
2024-05-24 22:15:54 -0500

    custom server error

--- errors/server-error.js
+++ errors/server-error.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require('http-status-codes');
 const CustomAPIError = require('./custom-api');
 
 class ServerError extends CustomAPIError {
+  // custom server error
   constructor(message) {
     super(message);
     this.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
